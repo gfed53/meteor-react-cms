3d8ae4e3b989d588db8dfd88f5d4d3e37dd8cf67
gfed53 <gfederico7153@gmail.com>
2018-02-23 12:47:12 -0500

    Post: added PostEdit component to be used within component, along with conditional rendering based on isEditing.

--- imports/ui/components/Post.jsx
+++ imports/ui/components/Post.jsx
@@ -1,23 +1,47 @@
 import React, { Component } from 'react';
 
 import MyEditor from './MyEditor.jsx';
+import PostEdit from './PostEdit.jsx';
 import { PostDisplay } from './PostDisplay';
 
 export default class Post extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      isEditing: false
+    }
+
+    this.onEdit = this.onEdit.bind(this);
   }
 
   // TODO: create an 'edit' button that reveals the PostEdit component, allowing user to update current post.
+  onEdit(){
+    this.setState({
+      isEditing: true
+    });
+  }
+
+
 
 
   render() {
+    const button = null;
+    const postEdit = null;
+
+    if(this.state.isEditing){
+      postEdit = <PostEdit draft_content={this.props.data.draft_content} />;
+    } else {
+      button = <button onClick={this.onEdit}>Edit</button>;
+    }
+
     return (
       <div>
         <p>By {this.props.data.author}</p>
         <p>Originally Posted: {this.props.data.date_posted.toLocaleString()}</p>
         <PostDisplay data={this.props.data.draft_content} />
+        {postEdit}
+        {button}
       </div>
     );
   }
