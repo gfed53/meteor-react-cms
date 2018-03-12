e262147d00947dae5dee63f74b184e0fa7b82db4
gfed53 <gfederico7153@gmail.com>
2018-03-12 14:24:55 -0400

    Cleaned up Post.jsx.

--- imports/ui/components/Post.jsx
+++ imports/ui/components/Post.jsx
@@ -14,12 +14,12 @@ export default class Post extends Component {
       isEditing: false
     }
 
-    this.handleEdit = this.handleEdit.bind(this);
+    this.handleEdit = this._handleEdit.bind(this);
     this.handleDelete = this._handleDelete.bind(this);
-    this.handleClose = this.handleClose.bind(this);
+    this.handleClose = this._handleClose.bind(this);
   }
 
-  handleEdit(){
+  _handleEdit(){
     this.setState({
       isEditing: true
     });
@@ -29,49 +29,46 @@ export default class Post extends Component {
     this.props.onDelete(this.props.data._id);
   }
 
-  // Closes PostEdit. This will be passed into PostEdit component (and hopefully will work..)
-  handleClose(){
+  _handleClose(){
     this.setState({
       isEditing: false
     });
   }
 
   getDraftHtml(raw){
-    const content = convertFromRaw(raw);
-    const options = {
-      defaultBlockTag: 'div',
-    };
+    const content = convertFromRaw(raw),
+          options = {
+            defaultBlockTag: 'div',
+          };
 
     return stateToHTML(content, options);
   }
 
-
-
-
   render() {
-    const button = null;
-    const postEdit = null;
-    const content = null;
-    const postButtons = this.props.data.owner && this.props.data.owner === Meteor.userId() ? (
-      <div className="post-button-container">
-        <button className="my-button" onClick={this.handleEdit}>Edit</button>
-        <button className="my-button" onClick={this.handleDelete}>Delete</button>
-      </div>
-    ) : null;
+    const button = null,
+          postEdit = null,
+          content = null,
+          postButtons = this.props.data.owner && this.props.data.owner === Meteor.userId() ? (
+            <div className="post-button-container">
+              <button className="my-button" onClick={this.handleEdit}>Edit</button>
+              <button className="my-button" onClick={this.handleDelete}>Delete</button>
+            </div>
+          ) : null;
 
     if(this.state.isEditing){
-      content = <PostEdit post_id={this.props.data._id} onSave={this.props.onSave} onClose={this.handleClose} draft_content={this.props.data.draft_content} />;
+      content = 
+      <PostEdit 
+      post_id={this.props.data._id} 
+      onSave={this.props.onSave} 
+      onClose={this.handleClose} 
+      draft_content={this.props.data.draft_content} />;
     } else {
       content = (
         <div>
-          <PostDisplay data={this.getDraftHtml(this.props.data.draft_content)} />
-          
+          <PostDisplay data={this.getDraftHtml(this.props.data.draft_content)} />     
           {postButtons}
         </div>
       );
-      // button = (<div className="post-button-edit-container">
-      //             <button className="my-button" onClick={this.handleEdit}>Edit</button>
-      //           </div>);
     }
 
     return (
@@ -81,13 +78,7 @@ export default class Post extends Component {
           <p>Originally Posted: {this.props.data.date_posted.toLocaleString()}</p>
           <p>Last Edited: {this.props.data.date_last_edited.toLocaleString()}</p>
         </div>
-
         {content}
-        
-        {/* <PostDisplay data={this.getDraftHtml(this.props.data.draft_content)} />
-        {postEdit}
-        {button} */}
-        
       </div>
     );
   }
