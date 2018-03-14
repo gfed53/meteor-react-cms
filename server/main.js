0fc06e22cecad8576a2e298e478fa9510d814d84
gfed53 <gfederico7153@gmail.com>
2018-03-14 14:59:27 -0400

    Accounts UI Wrapper: updated some styles.

--- server/main.js
+++ server/main.js
@@ -6,9 +6,6 @@ Meteor.startup(() => {
   // console.log('Meteor.settings',Meteor.settings);
   // console.log('MONGO_URL',process.env.MONGO_URL);
 
-  //----------- Configure mLab
-  // process.env.MONGO_URL = 'mongodb://gfed123:flob1357@ds215019.mlab.com:15019/cheesy-white-cms';
-
   console.log('Meteor.settings',Meteor.settings);
   console.log('MONGO_URL',process.env.MONGO_URL);
 });
