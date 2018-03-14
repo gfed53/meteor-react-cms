57317ecfc920888151dcf30e61692d573c4446c4
gfed53 <gfederico7153@gmail.com>
2018-03-14 12:29:25 -0400

    Created script to work with mLab account. User and pswd omitted for now.

--- server/main.js
+++ server/main.js
@@ -5,4 +5,10 @@ Meteor.startup(() => {
   // code to run on server at startup
   // console.log('Meteor.settings',Meteor.settings);
   // console.log('MONGO_URL',process.env.MONGO_URL);
+
+  //----------- Configure mLab
+  // process.env.MONGO_URL = 'mongodb://gfed123:flob1357@ds215019.mlab.com:15019/cheesy-white-cms';
+
+  console.log('Meteor.settings',Meteor.settings);
+  console.log('MONGO_URL',process.env.MONGO_URL);
 });
