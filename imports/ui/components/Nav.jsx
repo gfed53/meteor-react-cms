d99278f9a097ec8ed7d3a6df1428a37cea969683
gfed53 <gfederico7153@gmail.com>
2018-03-27 12:54:35 -0400

    Nav: added credits and github link.

--- imports/ui/components/Nav.jsx
+++ imports/ui/components/Nav.jsx
@@ -5,7 +5,13 @@ import AccountsUIWrapper from './AccountsUIWrapper.jsx';
 export const Nav = (props) => {
   return (
     <div className="nav-container">
-      <AccountsUIWrapper />
+      <div className="credits">
+        <span>Gregory Federico</span>
+        <a href="https://github.com/gfed53/meteor-react-cms" target="_blank"><i class="fa fa-github"></i></a>
+      </div>
+      <div>
+        <AccountsUIWrapper />
+      </div>
     </div>
   );
 };
