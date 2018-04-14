f4940562be7b0eb03efeb07d9f3ccc0aae4646fe
gfed53 <gfederico7153@gmail.com>
2018-04-14 17:11:05 -0400

    AccountsUIWrapper: made accounts link tab-accessible. Installed jQuery, added click handler to accounts link.

--- imports/ui/components/AccountsUIWrapper.jsx
+++ imports/ui/components/AccountsUIWrapper.jsx
@@ -2,12 +2,38 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Template } from 'meteor/templating';
 import { Blaze } from 'meteor/blaze';
+import $ from 'jquery';
  
 export default class AccountsUIWrapper extends Component {
   componentDidMount() {
     // Use Meteor Blaze to render login buttons
-    this.view = Blaze.render(Template.loginButtons,
+    this.view = Blaze.render(
+      Template.loginButtons,
       ReactDOM.findDOMNode(this.refs.container));
+
+    // Fix for tab accessibility
+    setTimeout(() => {
+      console.log(document.querySelectorAll('.login-link-text'));
+      let linkEls = document.querySelectorAll('.login-link-text');
+
+      linkEls.forEach((el) => {
+        console.log('el',el);
+        el.setAttribute('href','');
+        el.setAttribute('tabIndex',0);
+      });
+
+      // Focus test
+      $('.login-link-text').on('click', function(e) {
+        console.log('clicked login-link-text');
+      })
+
+      // Init keyboard trap
+
+
+
+
+    },1000);
+
   }
   componentWillUnmount() {
     // Clean up Blaze view
@@ -18,3 +44,6 @@ export default class AccountsUIWrapper extends Component {
     return <span ref="container" />;
   }
 }
+
+
+// Function for keyboard trap down here?
